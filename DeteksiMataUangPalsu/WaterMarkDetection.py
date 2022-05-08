4467a2d44737712b562d0ea52e99d1d697ecd1a5
Alb3rTan <101382309+Alb3rTan@users.noreply.github.com>
2022-05-08 14:14:32 +0700

    Update WaterMarkDetection.py

--- DeteksiMataUangPalsu/WaterMarkDetection.py
+++ DeteksiMataUangPalsu/WaterMarkDetection.py
@@ -1,44 +1,70 @@
 
+
 import glob
 import cv2 
 import numpy as np
 import imutils 
 
 
-def uang_matching():
+def WM_Detection():
     # load template
     template_data = []
-    template_files = glob.glob('Template/*.jpg', recursive=True)
+    template_files = glob.glob('template/WM5000.jpg', recursive=True)
     print("template loaded:", template_files)
     # prepare template
     for template_file in template_files:
         tmp = cv2.imread(template_file)
+
         # scalling
-        tmp = imutils.resize(tmp, width=int(tmp.shape[1]*0.5)) 
-        # grayscale 
+        tmp = imutils.resize(tmp, width=int(tmp.shape[1]*0.5))
+
+        # grayscale
         tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)  
-        kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-        #sharpening
-        tmp = cv2.filter2D(tmp, -1, kernel) 
+        	
+        (thresh, blackAndWhiteImage) = cv2.threshold(tmp, 135, 220, cv2.THRESH_BINARY)
+        	
+        cv2.imshow('Black white image', blackAndWhiteImage)
+        #kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+
+        # sharpening
+        #tmp = cv2.filter2D(tmp, -1, kernel)
+
         # smoothing
-        tmp = cv2.blur(tmp, (3, 3))  
-        # Edge with Canny 
-        tmp = cv2.Canny(tmp, 50, 200)  
-        nominal = template_file.replace('Template\\', '').replace('.jpg', '')
+        # blur
+        #tmp = cv2.blur(tmp, (1, 1))
+        # GaussianBlur
+        #tmp = cv2.GaussianBlur(tmp,(5,5),0)
+
+        # Edge 
+        # with Canny
+        #tmp = cv2.Canny(tmp, 100, 220)  
+        # with adaptiveThreshold
+        #tmp = cv2.adaptiveThreshold(tmp,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,5)
+
+        #tmp = imutils.rotate_bound(tmp,-90)
+               
+
+        nominal = template_file.replace('template\\', '').replace('.jpg', '')
         template_data.append({"glob":tmp, "nominal":nominal})
      
     # template matching
-    for image_glob in glob.glob('Test/*.jpg'):
+    for image_glob in glob.glob('test/5000.jpg'):
         for template in template_data:
             image_test = cv2.imread(image_glob)
             (tmp_height, tmp_width) = template['glob'].shape[:2]
-            cv2.imshow("Template", template['glob'])  
+            #cv2.imshow("Template Pembanding", template['glob'])  
 
             image_test_p = cv2.cvtColor(image_test, cv2.COLOR_BGR2GRAY) 
-            cv2.imshow("Step: Grayscal", image_test_p) 
+            #cv2.imshow("Step: Grayscal", image_test_p) 
+            (thresh, blackAndWhiteImageTest) = cv2.threshold(image_test_p, 135, 220, cv2.THRESH_BINARY)
+        	
+            cv2.imshow('Black white image test', blackAndWhiteImageTest)
 
-            image_test_p = cv2.Canny(image_test_p, 50, 200)
-            cv2.imshow("Step: edge with canny", image_test_p) 
+            #image_test_p = cv2.Canny(image_test_p, 50, 200)
+            #cv2.imshow("Step: edge with canny", image_test_p) 
+
+            #image_test_p = cv2.adaptiveThreshold(image_test_p,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,10)
+            #cv2.imshow("Step: edge with adaptiveThreshold", image_test_p)
 
             found = None
             thershold = 0.4
@@ -58,18 +84,20 @@ def uang_matching():
                     found = (maxVal, maxLoc, r)
                     if maxVal >= thershold: 
                         print("money:", template['nominal'], "detected")
-            if found is not None: 
-                (maxVal, maxLoc, r) = found
-                (startX, startY) = (int(maxLoc[0]*r), int(maxLoc[1] * r))
-                (endX, endY) = (
-                    int((maxLoc[0] + tmp_width) * r), int((maxLoc[1] + tmp_height) * r))
-                if maxVal >= thershold:
-                    cv2.rectangle(image_test, (startX, startY),
-                                  (endX, endY), (0, 0, 255), 2)
-                cv2.imshow("Result", image_test)
+
+                if found is not None: 
+                    (maxVal, maxLoc, r) = found
+                    (startX, startY) = (int(maxLoc[0]*r), int(maxLoc[1] * r))
+                    (endX, endY) = (
+                        int((maxLoc[0] + tmp_width) * r), int((maxLoc[1] + tmp_height) * r))
+                    if maxVal >= thershold:
+                        cv2.rectangle(image_test, (startX, startY),
+                                    (endX, endY), (0, 0, 255), 2)
+                    cv2.imshow("Result", image_test)
 
             cv2.waitKey(0)
 
 
 if __name__ == "__main__": 
-    uang_matching()
+    WM_Detection()
+
