a11b52f90c73d5041e73b63866fb1594451034aa
RobertCarlos412019054 <101856864+RobertCarlos412019054@users.noreply.github.com>
2022-05-12 16:01:52 +0700

    Update WaterMarkDetection.py

--- DeteksiMataUangPalsu/WaterMarkDetection.py
+++ DeteksiMataUangPalsu/WaterMarkDetection.py
@@ -8,11 +8,11 @@ import Main
 
 
 def WM_Detection():
-    # load WM template
+    #_load_WM_template
     WM_data = []    
     WM_files = glob.glob('template/'+Main.Tamplate_wm, recursive=True)
     print("Template WM loaded:", WM_files)
-    # prepare WM template==============================================================
+    #_prepare_WM_template==============================================================
     for WM_files in WM_files:
         tmp = cv2.imread(WM_files)        
         
@@ -20,60 +20,60 @@ def WM_Detection():
         tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
         cv2.imshow("Template Grayscal", tmp)
         
-        # Contrast & Brightness========================================================
+        #_Contrast_&_Brightness========================================================
         alpha = 0.7 # Contrast control (1.0-3.0)
         beta = 5 # Brightness control (0-100)
         tmp = cv2.convertScaleAbs(tmp, alpha=alpha, beta=beta)
         cv2.imshow("Template CB", tmp) 
         
-        # Black & White================================================================  	
+        #_Black_&_White================================================================  	
         #(thresh, blackAndWhiteImage) = cv2.threshold(tmp, 135, 200, cv2.THRESH_BINARY)        	
         #cv2.imshow('Black white image', blackAndWhiteImage)
         
-        # sharpening===================================================================
+        #_Sharpening===================================================================
         #kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])       
         #tmp = cv2.filter2D(tmp, -1, kernel)
         # smoothing====================================================================
         
-        # blur=========================================================================
+        #_Blur=========================================================================
         #tmp = cv2.blur(tmp, (1, 3))
-        # adaptiveThreshold============================================================
+        #_AdaptiveThreshold============================================================
         #tmp = cv2.adaptiveThreshold(tmp,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,5)
         
         #===========================================================================================
         WaterMark = WM_files.replace('WaterMark\\', '').replace('.jpg', '')
         WM_data.append({"glob_WM":tmp, "WaterMark":WaterMark})
         #===========================================================================================
-    # template matching
+    #_Template_matching
     for image_glob in glob.glob('test/'+Main.Image):
         for WM_data in WM_data:
             image_test_WM = cv2.imread(image_glob)
             (tmp_height, tmp_width) = WM_data['glob_WM'].shape[:2]            
-            # grayscale====================================================================
+            #_Grayscale====================================================================
             image_test_W = cv2.cvtColor(image_test_WM, cv2.COLOR_BGR2GRAY) 
             cv2.imshow("Image Grayscal", image_test_W)
-            # Contrast & Brightness========================================================
+            #_Contrast_&_Brightness========================================================
             alpha = 0.7 # Contrast control (1.0-3.0)
             beta = 5 # Brightness control (0-100)
             image_test_W = cv2.convertScaleAbs(image_test_W, alpha=alpha, beta=beta)
             cv2.imshow("Image CB", image_test_W)
-            # Black & White================================================================ 
+            #_Black_&_White================================================================ 
             #(thresh, blackAndWhiteImageTest) = cv2.threshold(image_test_W, 135, 250, cv2.THRESH_BINARY)        	
             #cv2.imshow('Black white image test', blackAndWhiteImageTest)
-            # sharpening===================================================================
+            #_Sharpening===================================================================
             #kernel1 = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
             #image_test_W = cv2.filter2D(image_test_W, -1, kernel1)
-            # smoothing====================================================================
+            #_Smoothing====================================================================
             # blur=========================================================================
             #image_test_W = cv2.blur(image_test_W, (1, 1))
-            # adaptiveThreshold============================================================
+            #_AdaptiveThreshold============================================================
             #image_test_W = cv2.adaptiveThreshold(image_test_W,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,10)
             #cv2.imshow("Step: edge with adaptiveThreshold", image_test_W)
             #===========================================================================================
             found = None
             thershold = 0.4
             for scale in np.linspace(0.2, 1.0, 20)[::-1]: 
-                # scalling uang
+                #_Scalling_uang
                 resized = imutils.resize(
                     image_test_W, width=int(image_test_W.shape[1] * scale))
                 r = image_test_W.shape[1] / float(resized.shape[1])                 
@@ -81,7 +81,7 @@ def WM_Detection():
                 if resized.shape[0] < tmp_height or resized.shape[1] < tmp_width:
                     break
 
-                # template matching
+                #_Template_matching
                 result = cv2.matchTemplate(resized, WM_data['glob_WM'], cv2.TM_CCOEFF_NORMED)
                 (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
                 if found is None or maxVal > found[0]:
