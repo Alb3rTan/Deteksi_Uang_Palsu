1ee4b82ea944a62dcbbf1616c04be1a765495535
RobertCarlos412019054 <101856864+RobertCarlos412019054@users.noreply.github.com>
2022-05-12 18:54:25 +0700

    Update LogoDetection.py

--- DeteksiMataUangPalsu/LogoDetection.py
+++ DeteksiMataUangPalsu/LogoDetection.py
@@ -8,53 +8,53 @@ import Main
 
 
 def Logo_Detection():
-    # load logo template
+    #_Load_logo_template
     logo_data = []
     logo_files = glob.glob('template/'+Main.Tamplate_lg, recursive=True)
     print("template Logo loaded:", logo_files)
-    # prepare logo template
+    #_Prepare_Logo_Template
     for logo_files in logo_files:
         tmp = cv2.imread(logo_files)        
-        # grayscale==========================================================
+        #_Grayscale==========================================================
         tmp = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
         cv2.imshow("Template Grayscale", tmp)                       
-        # sharpening=========================================================
+        #_Sharpening=========================================================
         kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])       
         tmp = cv2.filter2D(tmp, -1, kernel)
         cv2.imshow("Template Filter2D", tmp) 
-        # smoothing==========================================================
-        # blur===============================================================
+        #_Smoothing==========================================================
+        #_Blur===============================================================
         tmp = cv2.blur(tmp, (1, 3))  
         cv2.imshow("Template Blur", tmp)
-        # Edge===============================================================        
+        #_Edge===============================================================        
         # with Canny=========================================================
         #tmp = cv2.Canny(tmp, 100, 220) 
         #cv2.imshow("Template Cany", tmp)       
-        # with adaptiveThreshold=============================================
+        #_With_Adaptive_Threshold=============================================
         tmp = cv2.adaptiveThreshold(tmp,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,5)
         cv2.imshow("Template Pembanding", tmp) 
         #===========================================================================================
         Logo = logo_files.replace('Logo\\', '').replace('.jpg', '')
         logo_data.append({"glob_logo":tmp, "Logo":Logo})
 
-    # template matching
+    #_Template_Matching
     for image_glob in glob.glob('test/'+Main.Image):
         for template_logo in logo_data:
             image_test_logo = cv2.imread(image_glob)
             (tmp_height, tmp_width) = template_logo['glob_logo'].shape[:2]             
-            # grayscale==========================================================
+            #_Grayscale==========================================================
             image_test_l = cv2.cvtColor(image_test_logo, cv2.COLOR_BGR2GRAY)            
-            # sharpening=========================================================
+            #_Sharpening=========================================================
             kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
             image_test_l = cv2.filter2D(image_test_l, -1, kernel)
-            # smoothing==========================================================
-            # blur===============================================================
+            #_Smoothing==========================================================
+            #_Blur===============================================================
             image_test_l = cv2.blur(image_test_l, (1, 3))
-            # Edge===============================================================        
+            #_Edge===============================================================        
             # with Canny=========================================================
             #image_test_l = cv2.Canny(image_test_l, 50, 200)
             #cv2.imshow("Step: edge with canny", image_test_l)
-            # with adaptiveThreshold=============================================
+            #_With_Adaptive_Threshold=============================================
             image_test_l = cv2.adaptiveThreshold(image_test_l,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,10)
             cv2.imshow("Image adaptiveThreshold", image_test_l)
             #===========================================================================================
@@ -62,7 +62,7 @@ def Logo_Detection():
             thershold = 0.4
             
             for scale in np.linspace(0.2, 1.0, 20)[::-1]: 
-                # scalling uang
+                #_Scalling_Uang
                 resized = imutils.resize(
                     image_test_l, width=int(image_test_l.shape[1] * scale))
                 r = image_test_l.shape[1] / float(resized.shape[1]) 
@@ -71,7 +71,7 @@ def Logo_Detection():
                 if resized.shape[0] < tmp_height or resized.shape[1] < tmp_width:
                     break
 
-                # template matching
+                #_Template_Matching
                 result = cv2.matchTemplate(resized, template_logo['glob_logo'], cv2.TM_CCOEFF_NORMED)
                 (_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
                 if found is None or maxVal > found[0]:
